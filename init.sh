81aa9aad662677b937d09f6757ca9c70d257ab7f
Pavel Ivanov <ivpavig@gmail.com>
2013-05-29 01:48:56 +0400

    init script added

---
+++ init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# make links to sublime settings files
+ln -s ~/dotfiles/st2/Preferences.sublime-settings ~/.config/sublime-text-2/Packages/User/Preferences.sublime-settings
+ln -s ~/dotfiles/st2/Ruby.sublime-settings ~/.config/sublime-text-2/Packages/User/Ruby.sublime-settings
