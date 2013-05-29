01179955ec73bf4f1594432e4ad61e07356fe3e7
Pavel Ivanov <ivpavig@gmail.com>
2013-05-30 02:41:31 +0400

    tmux config added

--- init.sh
+++ init.sh
@@ -3,3 +3,6 @@
 # make links to sublime settings files
 ln -s ~/dotfiles/st2/Preferences.sublime-settings ~/.config/sublime-text-2/Packages/User/Preferences.sublime-settings
 ln -s ~/dotfiles/st2/Ruby.sublime-settings ~/.config/sublime-text-2/Packages/User/Ruby.sublime-settings
+
+# make link to tmux config
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
