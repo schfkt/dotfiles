c5144566b756bf5c97affac902f5fbdeb4942e7d
Pavel Ivanov <ivpavig@gmail.com>
2013-11-14 02:35:26 +0400

    Update init script

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@
 # make links to sublime settings files
 ln -s ~/dotfiles/st2/Preferences.sublime-settings ~/.config/sublime-text-2/Packages/User/Preferences.sublime-settings
 ln -s ~/dotfiles/st2/Ruby.sublime-settings ~/.config/sublime-text-2/Packages/User/Ruby.sublime-settings
+ln -s ~/dotfiles/st2/Default\ \(Linux\).sublime-keymap ~/.config/sublime-text-2/Packages/User/Default\ \(Linux\).sublime-keymap
 
 # make link to tmux config
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
@@ -12,4 +13,7 @@ ln -s ~/dotfiles/vimrc ~/.vimrc
 
 # link to urxvt config
 ln -s ~/dotfiles/Xdefaults ~/.Xdefaults
+
+# i3
+ln -s ~/dotfiles/i3 ~/.i3
 
