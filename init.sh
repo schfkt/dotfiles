9d7873f100ce2d48bb0f30e846ca5732c7816a05
Pavel Ivanov <ivpavig@gmail.com>
2013-12-28 20:27:49 +0400

    Add scripts

--- init.sh
+++ init.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-# make links to sublime settings files
+# st2
 ln -s ~/dotfiles/st2/Preferences.sublime-settings ~/.config/sublime-text-2/Packages/User/Preferences.sublime-settings
 ln -s ~/dotfiles/st2/Ruby.sublime-settings ~/.config/sublime-text-2/Packages/User/Ruby.sublime-settings
 ln -s ~/dotfiles/st2/Default\ \(Linux\).sublime-keymap ~/.config/sublime-text-2/Packages/User/Default\ \(Linux\).sublime-keymap
 
-# make link to tmux config
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-
-# make link to vim config
 ln -s ~/dotfiles/vimrc ~/.vimrc
-
-# link to urxvt config
 ln -s ~/dotfiles/Xdefaults ~/.Xdefaults
-
-# i3
 ln -s ~/dotfiles/i3 ~/.i3
-
-# xsessionrc
 ln -s ~/dotfiles/xsessionrc ~/.xsessionrc
+
+# binaries
+mkdir -p ~/bin
+ln -s ~/dotfiles/bin/scrshot ~/bin/scrshot
 
