b3a8df936f8aa7ab9884434b07cd6305381b78fa
Pavel Ivanov <ivpavig@gmail.com>
2014-03-02 17:46:00 +0400

    Update init script

--- init.sh
+++ init.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+DOTFILES_DIR=$(readlink -f `dirname $0`)
+cd $DOTFILES_DIR
 
 # init submodules
 git submodule init
 git submodule update
 
-# create links
+# sublime's configs
+for file in "$DOTFILES_DIR/st3/*"; do
+  ln -s $file ~/.config/sublime-text-3/Packages/User/
+done
+
+# other configs
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/Xdefaults ~/.Xdefaults
@@ -13,5 +20,6 @@ ln -s ~/dotfiles/xsessionrc ~/.xsessionrc
 
 # binaries
 mkdir -p ~/bin
-ln -s ~/dotfiles/bin/scrshot ~/bin/scrshot
-
+for file in "$DOTFILES_DIR/bin/*"; do
+  ln -s $file ~/bin/
+done
